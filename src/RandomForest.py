77e617db9a35241c64c4fd07f95d00f5048918fb
raymond sutrisno <raymond@sutrisno.me>
2019-10-27 00:58:12 -0500

    some shared modules for data_prep and RandomForest code

---
+++ src/RandomForest.py
@@ -0,0 +1,46 @@
+
+import pandas as pd
+import numpy as np
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from data_prep import (
+  read_dataset_from_csv,
+  preprocess_data_and_get_X_and_y,
+  partition_into_training_and_testing)
+
+def grid_search_for_best_random_forest(x_train, y_train):
+  base_model = RandomForestClassifier()
+  parameters = {
+    'n_estimators':[10, 20, 30, 40],
+    'criterion':['gini', 'entropy'],
+    'max_depth':[1, 2, 3, 4, None],
+    'max_leaf_nodes': [2, 4, 8, 16, None],
+    'min_samples_split': [2, 5, 10, None],
+    'min_samples_leaf': [1, 10, 30, 50, None],
+    'bootstrap': [True, False],
+    'random_state': [42]
+  }
+
+  grid_search_results = GridSearchCV(
+    estimator=base_model,
+    param_grid=parameters,
+    n_jobs=8,
+    return_train_score=True,
+    cv=10
+  ).fit(x_train, y_train)
+
+  return grid_search_results
+
+def __MAIN__(file_name):
+  data_set = read_dataset_from_csv(file_name)
+
+  X, y = preprocess_data_and_get_X_and_y(data_set)
+  X_train, X_test, y_train, y_test = partition_into_training_and_testing(X, y, random_state=0)
+
+  gridSearchObject = grid_search_for_best_random_forest(X_train, y_train)
+  print(gridSearchObject)
+
+if __name__ == '__main__':
+  import sys
+  __MAIN__(sys.argv[1])
--- src/data_prep.py
+++ src/data_prep.py
@@ -7,11 +7,34 @@ import pandas as pd
 import sklearn as sk
 import numpy as np
 
-def read_dataset(file_name: str):
+from sklearn.model_selection import train_test_split
+
+def read_dataset_from_csv(file_name: str):
   return pd.read_csv(file_name)
 
-def __MAIN_METHOD__(args: List[str]):
+def preprocess_data_and_get_X_and_y(data_set: pd.DataFrame):
+  def one_hot_encoder(sequence_str):
+    mapper = { # hardcoded for now
+      'A': '10000000',
+      'T': '01000000',
+      'C': '00100000',
+      'G': '00010000',
+      'N': '00001000',
+      'R': '00000100',
+      'S': '00000010',
+      'D': '00000001'
+    }
+    return list(''.join([mapper[base_pair] for base_pair in sequence_str]))
+
+  features = np.array(list(map(one_hot_encoder, data_set['Sequence'])))
+  labels = np.array(list(data_set['Class']))
+  return features, labels
+
+def partition_into_training_and_testing(X, y, random_state=0):
+  return train_test_split(X, y, test_size=0.2, random_state=random_state)
+
+def __MAIN__(args: List[str]):
   print(args)
 
 if __name__ == '__main__':
-  __MAIN_METHOD__(sys.argv)
+  __MAIN__(sys.argv)
