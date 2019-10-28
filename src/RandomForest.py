41d9c93bdcf5992b2149c3cd402e51d4a1160786
raymond sutrisno <raymond@sutrisno.me>
2019-10-28 12:32:21 -0500

    fixed NoneType float comparison error, turns out its the parameter search

--- src/RandomForest.py
+++ src/RandomForest.py
@@ -14,10 +14,10 @@ def grid_search_for_best_random_forest(x_train, y_train):
   parameters = {
     'n_estimators':[10, 20, 30, 40],
     'criterion':['gini', 'entropy'],
-    'max_depth':[1, 2, 3, 4, None],
-    'max_leaf_nodes': [2, 4, 8, 16, None],
-    'min_samples_split': [2, 5, 10, None],
-    'min_samples_leaf': [1, 10, 30, 50, None],
+    'max_depth':[1, 2, 3, 4, "None"],
+    'max_leaf_nodes': [2, 4, 8, 16],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 10, 30, 50],
     'bootstrap': [True, False],
     'random_state': [42]
   }
@@ -25,9 +25,10 @@ def grid_search_for_best_random_forest(x_train, y_train):
   grid_search_results = GridSearchCV(
     estimator=base_model,
     param_grid=parameters,
-    n_jobs=8,
     return_train_score=True,
-    cv=10
+    n_jobs=4,
+    cv=10,
+    error_score='raise'
   ).fit(x_train, y_train)
 
   return grid_search_results
@@ -36,8 +37,8 @@ def __MAIN__(file_name):
   data_set = read_dataset_from_csv(file_name)
 
   X, y = preprocess_data_and_get_X_and_y(data_set)
-  X_train, X_test, y_train, y_test = partition_into_training_and_testing(X, y, random_state=0)
 
+  X_train, X_test, y_train, y_test = partition_into_training_and_testing(X, y, random_state=0)
   gridSearchObject = grid_search_for_best_random_forest(X_train, y_train)
   print(gridSearchObject)
 
--- src/data_prep.py
+++ src/data_prep.py
@@ -8,26 +8,24 @@ import sklearn as sk
 import numpy as np
 
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
 
 def read_dataset_from_csv(file_name: str):
   return pd.read_csv(file_name)
 
 def preprocess_data_and_get_X_and_y(data_set: pd.DataFrame):
-  def one_hot_encoder(sequence_str):
-    mapper = { # hardcoded for now
-      'A': '10000000',
-      'T': '01000000',
-      'C': '00100000',
-      'G': '00010000',
-      'N': '00001000',
-      'R': '00000100',
-      'S': '00000010',
-      'D': '00000001'
-    }
-    return list(''.join([mapper[base_pair] for base_pair in sequence_str]))
+  features = np.array(list(map(list, data_set['Sequence'])), dtype='str')
+  labels = np.array(list(data_set['Class']), dtype='str').reshape(-1,1)
 
-  features = np.array(list(map(one_hot_encoder, data_set['Sequence'])))
-  labels = np.array(list(data_set['Class']))
+  base_pairs = ('A', 'C', 'D', 'G', 'N', 'R', 'S', 'T')
+  categories = [base_pairs for i in range(features.shape[1])]
+
+  features_encoder = OneHotEncoder(categories=categories).fit(features)
+  labels_encoder = OneHotEncoder(handle_unknown='ignore').fit(labels)
+
+  features = features_encoder.transform(features).toarray()
+  labels = labels_encoder.transform(labels).toarray()
+
   return features, labels
 
 def partition_into_training_and_testing(X, y, random_state=0):
