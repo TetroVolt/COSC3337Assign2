4ac2121f5a4deee46eac04ffddd15af16c486f2c
ColbyK <colbyk44@gmail.com>
2019-10-29 04:23:51 -0500

    expanded SVM

--- src/SVM.py
+++ src/SVM.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from sklearn.svm import SVC
 from random import sample
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+import numpy as np
 
 def to_int_map(c):
     if c == 'A':
@@ -55,6 +58,7 @@ def getRandSample(size, ratio):
             outSample.append(item)
     return [ratioSample, outSample]
 
+"""
 dataset = pd.read_csv('..\\datasetSPLICEDNA\\splice.data.csv', header=None, skiprows=1)
 solutions = [solutions_map(tempo) for tempo in dataset[0]]
 rawDNA = dataset[2]
@@ -76,16 +80,90 @@ for index in splitData[1]:
     testSol.append(solutions[index])
 
 # SVM method
-clf = SVC(gamma='auto')
+clf = SVC(kernel='poly', degree=8, gamma='auto')
 # Use training set to fit model
 clf.fit(trainDNA, trainSol)
 # create prediction with testing set
 pred = clf.predict(testDNA)
 # compare testing predictions with solutions
 print(accuracy(pred, testSol))
-
 # Train and test on the same set
-clf2 = SVC(gamma='auto')
+clf2 = SVC(kernel='poly', degree=8, gamma='auto')
 clf2.fit(DNA, solutions)
 pred2 = clf2.predict(DNA)
 print(accuracy(pred2, solutions))
+
+#
+#
+#
+#
+#
+"""
+from data_prep import (
+  read_dataset_from_csv,
+  preprocess_data_and_get_X_and_y,
+  partition_into_training_and_testing)
+
+
+newDataset = read_dataset_from_csv('..\\datasetSPLICEDNA\\splice.data.csv')
+X, y = preprocess_data_and_get_X_and_y(newDataset)
+X_train, X_test, y_train, y_test = partition_into_training_and_testing(X, y)
+
+y_train_format = []
+for item in y_train:
+    if(item[0] == 1):
+        y_train_format.append(0)
+    elif(item[1] == 1):
+        y_train_format.append(1)
+    elif(item[2] == 1):
+        y_train_format.append(2)
+    else:
+        print("FORMAT ERROR")
+
+y_test_format = []
+for item in y_test:
+    if(item[0] == 1):
+        y_test_format.append(0)
+    elif(item[1] == 1):
+        y_test_format.append(1)
+    elif(item[2] == 1):
+        y_test_format.append(2)
+    else:
+        print("FORMAT ERROR")    
+
+#print(X_train[:5])
+#print(y_train[0][0])
+#print(y_train[0])
+
+print(y_train_format[:10])
+
+clf3 = SVC(random_state=0)#.fit(X_train, y_train_format)
+#score = cross_val_score(clf3, X_train, y_train_format, cv=10, n_jobs=4)
+#print(score)
+#print(clf3.score(X_test, y_test_format))
+
+param_grid = [
+  {'C': [1, 10, 100, 1000], 
+   'gamma': [0.01, 0.001, 0.0001, 'auto'], 
+   'kernel': ['linear','poly','rbf'], 
+   'degree':[2,3,4,5,6,7,8]},
+ ]
+
+grid = GridSearchCV(estimator=clf3, 
+                    param_grid=param_grid,
+                    return_train_score=True,
+                    error_score='raise',
+                    n_jobs=4,
+                    cv=10
+                    ).fit(X_train, y_train_format)
+
+print(grid)
+print(grid.score(X_test, y_test_format))
+
+best_est = grid.best_estimator_
+test_score = best_est.score(X_test, y_test_format)
+print(test_score)
+#clf3.fit(X_train, y_train_format)
+  
+print("done")
+
